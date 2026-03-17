48d0d9160c89da9d474ff15fa46838a9124b7cc4
Desmond1up <153213527+Desmond1up@users.noreply.github.com>
2026-03-17 16:28:36 +0000

    Added NetBeans project and initial classes
    Added NetBeans project and initial classes

---
+++ ChargingHubManager/src/Model/Device.java
@@ -0,0 +1,23 @@
+package model;
+
+public abstract class Device {
+
+    protected String deviceID;
+    protected int batteryLevel;
+
+    public Device(String deviceID, int batteryLevel) {
+        this.deviceID = deviceID;
+        this.batteryLevel = batteryLevel;
+    }
+
+    public void startCharging() {
+        System.out.println("Charging started for device: " + deviceID);
+    }
+
+    public void stopCharging() {
+        System.out.println("Charging stopped for device: " + deviceID);
+    }
+
+    public abstract int calculateChargingTime();
+
+}
